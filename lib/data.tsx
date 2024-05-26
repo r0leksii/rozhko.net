7d40af603c4764b88658d4215cc9aa18a0aab505
r0leksii <rozhkoaleks@gmail.com>
2024-05-26 16:21:14 +0300

    edit email

--- lib/data.tsx
+++ lib/data.tsx
@@ -48,7 +48,7 @@ export const userProfile: UserProfile = {
       label: 'linkedin.com/in/rozhkoaleks',
     },
     resume: {
-      href: '/resume.pdf',
+      href: '/cv-oleksii-rozhko.pdf',
       icon: <ArrowRightIcon />,
       label: 'View full resume',
     },
